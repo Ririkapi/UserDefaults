bb151393662447580d49470d5116f6424f36b98d
Ririkapi <lilyflower0425@gmail.com>
2018-06-15 00:35:54 +0900

    追加課題　再提出 メソッド

--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    var dic:[String:String] = [:]
+    let userDefault = UserDefaults.standard
+    
+    @IBOutlet var fruit: UITextField!
+    @IBOutlet var vege: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
+        if userDefault.dictionary(forKey: "Dictionary") != nil{
+            dic = userDefault.dictionary(forKey: "Dictionary") as! Dictionary<String, String>
+            fruit.text = dic["F"]
+            vege.text = dic["V"]
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func save(){
+        dic["F"] = fruit.text
+        dic["V"] = vege.text
+        
+        userDefault.set(dic, forKey: "Dictionary")
     }
-
-
 }
-
